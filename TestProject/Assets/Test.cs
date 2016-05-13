3af83176341c743445ff6484c816582eea6184bb
zomzhi <zomzhi@yeah.net>
2016-05-13 16:29:10 +0800

    * Assets\Test.cs:   First commit
    * TestProject.sln:

--- TestProject/Assets/Test.cs
+++ TestProject/Assets/Test.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Test : MonoBehaviour {
+public class Test : MonoBehaviour
+{
+
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		Debug.Log ("This is a git test project");
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 }
